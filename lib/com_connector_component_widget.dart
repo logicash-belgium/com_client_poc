bb2975bfaef38069b1e1b705ea664f116db71401
Guillaume Maiano <guillaume@maiano.fr>
2021-07-29 15:53:04 +0200

    Adds some demo code

--- lib/com_connector_component_widget.dart
+++ lib/com_connector_component_widget.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:libserialport/libserialport.dart';
 
 class ComConnectorComponentWidget extends StatefulWidget {
 
@@ -12,6 +15,24 @@ class ComConnectorComponentWidget extends StatefulWidget {
 }
 
 class _ComConnectorComponentWidgetState extends State<ComConnectorComponentWidget> {
+
+  final name = SerialPort.availablePorts.first;
+
+  void showReadPort() {
+    final port = SerialPort(name);
+    if (!port.openReadWrite()) {
+      print(SerialPort.lastError);
+      exit(-1);
+    }
+
+    //port.write(/* ... */);
+
+    final reader = SerialPortReader(port);
+    reader.stream.listen((data) {
+      print('received: $data');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextField(
