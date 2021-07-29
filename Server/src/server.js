74ca925824d2f19101986d5d84489de7cb2188e5
Guillaume Maiano <guillaume@maiano.fr>
2021-07-29 14:54:13 +0200

    Docker incompatibilities
    Apparently only Windows machiens can access COM ports (?).
    TBD, because I'm not a Win guy

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,4 +1,8 @@
 var SerialPort = require('serialport');
+// Note: this cannot work within Docker on a Node-based image on Windows, since only Windows containers have access to COM ports, and only within the procedure explained underneath
+// https://docs.microsoft.com/en-us/virtualization/windowscontainers/deploy-containers/hardware-devices-in-containers
+// 86E0D1E0-8089-11D0-9CE4-08003E301F73 --> COM ports
+// docker run --isolation=process --device="class/{interface class GUID}" mcr.microsoft.com/windows/servercore:1809
 const port = new SerialPort('COM1', {baudRate: 9600});
 
 var app = require('express')();
